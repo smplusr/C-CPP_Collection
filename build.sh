f9e8ec0278c6dde93a368c9546197ae784717545
smplusr <83000221+smplusr@users.noreply.github.com>
2021-05-19 19:27:58 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-clear
-
-g++ -c -Wall -std=c++17 src/*.cpp -Iinclude -DSFML_STATIC
-g++ -o latest_binary *.o -Wl,-rpath,lib -Llib -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window
-
-mv latest_binary bin
-rm -r *.o
