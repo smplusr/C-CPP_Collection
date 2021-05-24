4c9b3bda622c6b8e2cf09bbbf7a0ddb3a18d468d
smplusr <83000221+smplusr@users.noreply.github.com>
2021-05-24 16:54:25 +0200

    Update build.sh

--- sfml-base-project/build.sh
+++ sfml-base-project/build.sh
@@ -3,7 +3,7 @@
 clear
 
 g++ -c -Wall -std=c++17 src/*.cpp -Iinclude -DSFML_STATIC
-g++ -o latest_binary *.o -Wl,-rpath,lib -Llib -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window
+g++ -o latest_binary *.o -Wl,-rpath="$ORIGIN/../lib" -Llib -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window
 
 mv latest_binary bin
 rm -r *.o
