b481c4f93b58240636f9eb504b7f039c6d22351d
smplusr <83000221+smplusr@users.noreply.github.com>
2021-06-08 00:59:06 +0200

    Add files via upload

--- sfml-example-console/src/assets/model.h
+++ sfml-example-console/src/assets/model.h
@@ -3,6 +3,9 @@ class Plane
 	public:
 		void draw()
 		{
+			glPushMatrix();
+			glRotatef(0.f, 0.f, 0.f, 0.f);
+			glTranslatef(0.f, 0.f, 0.f);
 			glBegin(GL_QUADS);
 				glColor3f(0.8f, 0.8f, 0.8f);
 				glVertex3f(-1.f, -1.f, -1.f);
@@ -10,10 +13,9 @@ class Plane
 				glVertex3f(1.f, -1.f, 1.f);
 				glVertex3f(-1.f, -1.f, 1.f);
 			glEnd();
+			glPopMatrix();
 		}
-		void test(std::string message){std::cout << message << std::endl;}
 };
-
 
 class Cube
 {
@@ -24,14 +26,14 @@ class Cube
 	void update()
 	{	
 		if(!m_placed)
-			glTranslatef(0.0f, 0.0, -1.33f);
+			glTranslatef(0.f, 0.f, -1.33f);
 		m_placed = true;
 
 		m_angle += 0.4;
 		glPushMatrix();	
-		glRotatef(m_angle, 1.0f, 0.0f, 0.0f);
-		glRotatef(m_angle, 0.0f, 1.0f, 0.0f);
-		glRotatef(m_angle, 0.0f, 0.0f, 1.0f);
+		glRotatef(m_angle, 1.f, 0.f, 0.f);
+		glRotatef(m_angle, 0.f, 1.f, 0.f);
+		glRotatef(m_angle, 0.f, 0.f, 1.f);
 		draw();
 		glPopMatrix();
 	}
@@ -40,46 +42,46 @@ class Cube
 		glBegin(GL_QUADS);
 	 
 			// Top face
-		  	glColor3f(   0.5f, 0.0f,  0.5f );  // Purple
-		  	glVertex3f(  0.35f, 0.35f, -0.35f );  // Top-right of top face
-		  	glVertex3f( -0.35f, 0.35f, -0.35f );  // Top-left of top face
-		  	glVertex3f( -0.35f, 0.35f,  0.35f );  // Bottom-left of top face
-		 	glVertex3f(  0.35f, 0.35f,  0.35f );  // Bottom-right of top face
+		  	glColor3f(   1.f, 0.f,  1.f );  // Pink
+		  	glVertex3f(  .35f, .35f, -.35f );  // Top-right of top face
+		  	glVertex3f( -.35f, .35f, -.35f );  // Top-left of top face
+		  	glVertex3f( -.35f, .35f,  .35f );  // Bottom-left of top face
+		 	glVertex3f(  .35f, .35f,  .35f );  // Bottom-right of top face
 		 
 			// Bottom face
-			glColor3f(   0.0f,  1.0f,  0.0f ); // Green
-			glVertex3f(  0.35f, -0.35f, -0.35f ); // Top-right of bottom face
-			glVertex3f( -0.35f, -0.35f, -0.35f ); // Top-left of bottom face
-			glVertex3f( -0.35f, -0.35f,  0.35f ); // Bottom-left of bottom face
-			glVertex3f(  0.35f, -0.35f,  0.35f ); // Bottom-right of bottom face
+			glColor3f(   .0f,  1.f,  .0f ); // Green
+			glVertex3f(  .35f, -.35f, -.35f ); // Top-right of bottom face
+			glVertex3f( -.35f, -.35f, -.35f ); // Top-left of bottom face
+			glVertex3f( -.35f, -.35f,  .35f ); // Bottom-left of bottom face
+			glVertex3f(  .35f, -.35f,  .35f ); // Bottom-right of bottom face
 		 
 			// Front face
-			glColor3f(   0.0f,  0.0f, 1.0f );  // Blue
-			glVertex3f(  0.35f,  0.35f, 0.35f );  // Top-Right of front face
-			glVertex3f( -0.35f,  0.35f, 0.35f );  // Top-left of front face
-			glVertex3f( -0.35f, -0.35f, 0.35f );  // Bottom-left of front face
-			glVertex3f(  0.35f, -0.35f, 0.35f );  // Bottom-right of front face
+			glColor3f(   .0f,  .0f, 1.f );  // Blue
+			glVertex3f(  .35f,  .35f, .35f );  // Top-Right of front face
+			glVertex3f( -.35f,  .35f, .35f );  // Top-left of front face
+			glVertex3f( -.35f, -.35f, .35f );  // Bottom-left of front face
+			glVertex3f(  .35f, -.35f, .35f );  // Bottom-right of front face
 		 
 			// Back face
-			glColor3f(   0.5f,  0.5f,  0.0f ); // Orange
-			glVertex3f(  0.35f, -0.35f, -0.35f ); // Bottom-Left of back face
-			glVertex3f( -0.35f, -0.35f, -0.35f ); // Bottom-Right of back face
-			glVertex3f( -0.35f,  0.35f, -0.35f ); // Top-Right of back face
-			glVertex3f(  0.35f,  0.35f, -0.35f ); // Top-Left of back face
+			glColor3f(   1.f,  1.f,  0.f ); // Yellow
+			glVertex3f(  .35f, -.35f, -.35f ); // Bottom-Left of back face
+			glVertex3f( -.35f, -.35f, -.35f ); // Bottom-Right of back face
+			glVertex3f( -.35f,  .35f, -.35f ); // Top-Right of back face
+			glVertex3f(  .35f,  .35f, -.35f ); // Top-Left of back face
 		 
 			// Left face
-			glColor3f(   0.0f,  0.0f,  0.0f);  // Black
-			glVertex3f( -0.35f,  0.35f,  0.35f);  // Top-Right of left face
-			glVertex3f( -0.35f,  0.35f, -0.35f);  // Top-Left of left face
-			glVertex3f( -0.35f, -0.35f, -0.35f);  // Bottom-Left of left face
-			glVertex3f( -0.35f, -0.35f,  0.35f);  // Bottom-Right of left face
+			glColor3f(   0.f,  1.f,  1.f);  // Cyan
+			glVertex3f( -.35f,  .35f,  .35f);  // Top-Right of left face
+			glVertex3f( -.35f,  .35f, -.35f);  // Top-Left of left face
+			glVertex3f( -.35f, -.35f, -.35f);  // Bottom-Left of left face
+			glVertex3f( -.35f, -.35f,  .35f);  // Bottom-Right of left face
 		 
 			// Right face
-			glColor3f(   1.0f,  0.0f,  0.0f);  // Red
-			glVertex3f(  0.35f,  0.35f,  0.35f);  // Top-Right of left face
-			glVertex3f(  0.35f,  0.35f, -0.35f);  // Top-Left of left face
-			glVertex3f(  0.35f, -0.35f, -0.35f);  // Bottom-Left of left face
-			glVertex3f(  0.35f, -0.35f,  0.35f);  // Bottom-Right of left face
+			glColor3f(   1.f,  0.f,  0.f);  // Red
+			glVertex3f(  .35f,  .35f,  .35f);  // Top-Right of left face
+			glVertex3f(  .35f,  .35f, -.35f);  // Top-Left of left face
+			glVertex3f(  .35f, -.35f, -.35f);  // Bottom-Left of left face
+			glVertex3f(  .35f, -.35f,  .35f);  // Bottom-Right of left face
 		
 		// End of GL calls
 		glEnd();
