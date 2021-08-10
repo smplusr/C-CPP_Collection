09826ae138b29a872a452a2db2fb4d7216417912
smplusr <83000221+smplusr@users.noreply.github.com>
2021-08-10 16:14:09 +0200

    Add files via upload

--- egl-x11-example-triangle/main.c
+++ egl-x11-example-triangle/main.c
@@ -1,1 +1,22 @@
-int main(){}
+/* gcc main.c -lX11 -lEGL -lGESv2 -o a.out */
+
+#include"display.h"
+#include"triangle.h"
+
+int main(int argc, char *argv[])
+{
+	const unsigned int width = 640,height = 480;
+	struct display *display;
+	struct window *window;
+
+	display=display_open();
+	window=window_create(display, argv[0], 0, 0, width, height);
+
+	window_show(window);
+	DrawTriangle(window);
+	sleep(10);
+
+	window_close(window);
+	display_close(display);
+	return 0;
+}
